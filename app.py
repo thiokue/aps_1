7eba20361194dee3de2195bc74fe6a799770075e
Thiago Okuyama <thiokue@gmail.com>
2023-04-10 17:36:04 -0300

    Started working on the web side of the project

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from scrape import get_data
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    df = get_data()
+    return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scrape.py
+++ scrape.py
@@ -1,30 +1,46 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
-
-driver = webdriver.Chrome()
-
-driver.get('https://www.tripadvisor.com.br/Attractions-g2628648-Activities-c61-Vale_do_Paraiba_State_of_Sao_Paulo.html')
-
-page = driver.page_source
-
-soup = BeautifulSoup(page, 'html.parser')
-
-names = soup.find_all(class_='XfVdV o AIbhI')
-names = [i.text for i in names]
-names = [i[3:] for i in names]
-
-desc = soup.find_all(class_='BKifx')
-desc = [i.text for i in desc]
+from selenium.webdriver.chrome.options import Options
 
 
-dic = {
-    'Título': [i for i in names],
-    'Descriçao': [i for i in desc]
-}
+def get_data():
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
 
-print(pd.DataFrame(dic))
+    driver.get('https://www.tripadvisor.com.br/Attractions-g2628648-Activities-c61-Vale_do_Paraiba_State_of_Sao_Paulo.html')
 
-driver.quit()
+    page = driver.page_source
 
+    soup = BeautifulSoup(page, 'html.parser')
+
+    names = soup.find_all(class_='XfVdV o AIbhI')
+    names = [i.text for i in names]
+    names = [i[3:] for i in names]
+
+    desc = soup.find_all(class_='BKifx')
+    desc = [i.text for i in desc]
+
+    comments = soup.find_all('span', class_='biGQs _P pZUbB osNWb')
+    comments = [i.text for i in comments]
+
+    rating_raw = soup.find_all(class_='jVDab o W f u w JqMhy')
+    rating = soup.find_all('svg', {'class': 'UctUV d H0 hzzSG'})
+    rating = [i.get('aria-label') for i in rating]
+    del rating[0]
+    rating = [i[:3] for i in rating]
+
+    dic = {
+        'Título': [i for i in names],
+        'Num Comentários': [i for i in comments],
+        'Rating': [i for i in rating],
+        'Descriçao': [i for i in desc],
+    }
+
+    df = pd.DataFrame(dic)
+
+
+    driver.quit()
+    return df
 
